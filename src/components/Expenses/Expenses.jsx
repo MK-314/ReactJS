4310c63c339b5569d5b985f381726386248cb249
Mikhail Kashkov <mikhail.kashkov@student.vcc.ca>
2021-05-10 22:24:14 -0700

    my version of filter

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -9,11 +9,18 @@ function Expenses({expenses}){
     var filterHandler = selectedYear =>{
         setFilteredYear(selectedYear);
     }
+    var returnArr = [];
     return(
         <div>
             <Card className='expenses'>
                 <ExpensesFilter selected={filteredYear} onCustomFilter={filterHandler}/>
-                {expenses.map(expenses => <ExpenseItem key={expenses.id} title={expenses.title} amount={expenses.amount} date={expenses.date}></ExpenseItem>)}
+                {/* {expenses.map(expenses => <ExpenseItem key={expenses.id} title={expenses.title} amount={expenses.amount} date={expenses.date}></ExpenseItem>)} */}
+                {expenses.forEach(element => {
+                    if(element.date.getFullYear().toString() === filteredYear){
+                        returnArr.push(<ExpenseItem key={element.id} title={element.title} amount={element.amount} date={element.date}></ExpenseItem>);
+                    }
+                })}
+                {returnArr}
             </Card>
         </div>
     );
